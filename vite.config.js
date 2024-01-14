ec2358fb85d21b383bdcf4afa0ef1259dfbaf124
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-14 06:59:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()
-    ,chunkSplitPlugin()],
-  build: {
-    chunkSizeWarningLimi: 1600,
+  plugins: [react(),chunkSplitPlugin()],
+  build:{
+    chunkSizeWarningLimi:3600,
   }
 })
