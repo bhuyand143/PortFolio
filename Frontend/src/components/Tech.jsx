02fc590a9dde25b71b579a3ca4723db321912c60
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-14 18:45:49 +0530

    updated

--- Frontend/src/components/Tech.jsx
+++ Frontend/src/components/Tech.jsx
@@ -3,18 +3,16 @@ import { BallCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
 
-
 const Tech = () => {
   return (
-    <div className=' flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology)=>(
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon}/>
-            <p className='text-white text-center'>{technology.name}</p>
+          <BallCanvas icon={technology.icon}/>
+          <p className='text-white text-center'>{technology.name}</p>
         </div>
       ))}
     </div>
-  )
-}
-
+  );
+};
 export default SectionWrapper(Tech,"");
