4a671e584e20ced2ab2aff8bf4c330f44286f0bc
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-14 20:12:31 +0530

    updated

--- Frontend/src/components/Works.jsx
+++ Frontend/src/components/Works.jsx
@@ -6,7 +6,7 @@ import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
-import { useMediaQuery } from 'react-responsive';
+
 
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
@@ -37,37 +37,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
   )
 }
 
-const PmobileCard = ({ index, name, description, tags, image, source_code_link }) => {
-  return (
-    <div>
-      <div
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-        <div className='relative w-full h-[230px]'>
-          <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' />
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
-              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain' />
-            </div>
-          </div>
-        </div>
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
 const Works = () => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isDesktop = useMediaQuery({ minWidth: 1024 });
 
   return (
     <>
@@ -81,11 +51,7 @@ const Works = () => {
         </p>
       </div>
       <div className='mt-20 flex flex-wrap gap-7'>
-        {(isMobile||isTablet) && projects.map((project, index) => (
-          <PmobileCard key={`project-${index}`} index={index} {...project} />
-        ))}
-
-        { isDesktop &&projects.map((project, index) => (
+        {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
