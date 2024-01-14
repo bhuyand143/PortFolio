842ce6d6d1078690c7038ec0e3d5e012ec425323
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-14 06:44:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{chunkSizeWarningLimi:1600,}
+  build:{
+    chunkSizeWarningLimi:1600,
+  }
 })
