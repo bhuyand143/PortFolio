9105b9d06a524358e1c03484a582a3db8435265e
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-14 18:49:03 +0530

    Update Stars.jsx

--- Frontend/src/components/canvas/Stars.jsx
+++ Frontend/src/components/canvas/Stars.jsx
@@ -7,7 +7,7 @@ import { inSphere } from 'maath/random/dist/maath-random.esm';
 const Stars = (props) => {
   const ref=useRef();
 
-  const sphere=inSphere(new Float32Array(4500),{radius:1.2})
+  const sphere=inSphere(new Float32Array(5000),{radius:1.2})
 
   useFrame((state,delta)=>{
      ref.current.rotation.x-=delta/10;
