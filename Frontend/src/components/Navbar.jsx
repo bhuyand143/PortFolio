b77c5f4c802fcfa362473e4fb41eb15cefbfd4a5
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-23 13:51:07 +0530

    navbar updated

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <img src={log} alt='logo' className="w-9 h-9 object-contain" />
             <p className='text-white text-[18px] font-bold cursor-pointer flex'>
               Dibyajyoti &nbsp;
-              <span className='sm:block hidden'>| Bhuyan</span></p>
+              <span className='md:block hidden'>| Bhuyan</span></p>
           </Link>
           <ul className='list-none hidden sm:flex flex-row gap-10'>
             {navLinks.map((link) => (
